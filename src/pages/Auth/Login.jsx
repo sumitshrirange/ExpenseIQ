bcd2751502a975ad78862372368a1dfddf921910
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-12 11:41:50 +0530

    login api integration commit

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,6 +3,8 @@ import AuthLayout from "../../layouts/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -28,6 +30,25 @@ function Login() {
     setError("");
 
     // Login API call
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong! Please try again.");
+      }
+    }
   };
 
   return (
