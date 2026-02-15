cb97875b89a8ab86b6f4df36c1df863df80354f7
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-15 13:16:38 +0530

    upload image cloudinary commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,10 +4,6 @@ import { BASE_URL } from "./apiPaths";
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
 });
 
 // Request interceptor
--- src/utils/uploadImage.js
+++ src/utils/uploadImage.js
@@ -3,25 +3,18 @@ import axiosInstance from "./axiosInstance";
 
 const uploadImage = async (imageFile) => {
   const formData = new FormData();
-
-  // Append image file to form data
   formData.append("image", imageFile);
 
   try {
     const response = await axiosInstance.post(
       API_PATHS.IMAGE.UPLOAD_IMAGE,
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data", // Set header for file upload
-        },
-      },
+      formData
     );
 
-    return response.data; // Return response data
+    return response.data;
   } catch (error) {
     console.error("Error uploading the image:", error);
-    throw error; // Rethrow error for handling
+    throw error;
   }
 };
 
