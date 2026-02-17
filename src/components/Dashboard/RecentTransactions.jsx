e3ee5672c282821402a4c573d50d594e085fcd1c
Sumit Shrirange <sumitshrirange81@gmail.com>
2026-02-17 11:17:07 +0530

    update feature commit

--- src/components/Dashboard/RecentTransactions.jsx
+++ src/components/Dashboard/RecentTransactions.jsx
@@ -15,21 +15,17 @@ const RecentTransactions = ({ transactions, onSeeMore }) => {
       </div>
 
       <div className="mt-6">
-        {transactions
-          ?.slice()
-          ?.reverse()
-          ?.slice(0, 4)
-          ?.map((item) => (
-            <TransactionInfoCard
-              key={item._id}
-              title={item.type == "Expense" ? item.category : item.source}
-              icon={item.icon}
-              date={moment(item.date).format("DD MMM YYYY")}
-              amount={item.amount}
-              type={item.type}
-              hideDeleteBtn
-            />
-          ))}
+        {transactions?.slice(0, 4)?.map((item) => (
+          <TransactionInfoCard
+            key={item._id}
+            title={item.type == "Expense" ? item.category : item.source}
+            icon={item.icon}
+            date={moment(item.date).format("DD MMM YYYY")}
+            amount={item.amount}
+            type={item.type}
+            hideDeleteBtn
+          />
+        ))}
       </div>
     </div>
   );
